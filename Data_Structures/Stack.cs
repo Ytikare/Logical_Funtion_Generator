755fdecb6197cbb65317751f2222aa69939990c3
Ytikare <ytikare2@tutanota.com>
2023-12-10 16:13:03 +0200

    Created a Stack

---
+++ Data_Structures/Stack.cs
@@ -0,0 +1,64 @@
+﻿namespace Data_Structures
+{
+    public class Stack<T>
+    {
+        private T[] values;
+
+        private int index = -1;
+
+        public int Coutn => values.Length;
+
+        public Stack()
+        {
+            values = new T[4];
+        }
+
+        public Stack(int lenght)
+        {
+            values = new T[lenght];
+        }
+
+        public T Peek()
+        {
+            if (index == -1)
+            {
+                return default(T);
+            }
+            return values[index];
+        }
+
+        public T Pop()
+        {
+            if (index == -1)
+            {
+                return default(T);
+            }
+
+            index -= 1;
+
+            return values[index + 1];
+        }
+
+        public void Push(T value)
+        {
+            index++;
+
+            if (index == values.Length)
+            {
+                ExpandValues();
+            }
+
+            this.values[index] = value;
+        }
+
+        private void ExpandValues()
+        {
+            var newCollection = new T[values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                newCollection[i] = values[i];
+            }
+        }
+    }
+}
